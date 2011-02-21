4010a2adbc792a76e0b03fef94caf34cd27caaa3
Dan Hoerr <dan@autumnrayne.net>
2011-02-20 22:37:06 -0500

    removed default index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 SampleApp::Application.routes.draw do
+  match '/about' => 'pages#about'
+   match '/contact' => 'pages#contact'
+   match '/help' => 'pages#help'
+
+   root :to => 'pages#home'
+   
   get "pages/home"
   get "pages/contact"
   get "pages/about"
   get "pages/help"
 
+ 
+ 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
